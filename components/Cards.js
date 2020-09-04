3a07abe7e1abfaa76bd4cf9f6724149e00b298bc
Elizabeth Drumm <elizabethdrumm@Elizabeths-MacBook-Pro.local>
2020-09-04 12:33:55 -0500

    edits

--- components/Cards.js
+++ components/Cards.js
@@ -1,22 +1,71 @@
-// STEP 3: Create article cards.
-// -----------------------
-// Send an HTTP GET request to the following address: https://lambda-times-api.herokuapp.com/articles
-// Study the response data you get back, closely.
-// You will be creating a card for each article in the response.
-// This won't be as easy as just iterating over an array though.
-//
-// Write a function that takes a single article object and returns the following markup:
-//
-// <div class="card">
-//   <div class="headline">{Headline of article}</div>
-//   <div class="author">
-//     <div class="img-container">
-//       <img src={url of authors image} />
-//     </div>
-//     <span>By {author's name}</span>
-//   </div>
-// </div>
-//
-// Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-//
-// Use your function to create a card for each of the articles, and append each card to the DOM.
+
+// // STEP 3: Create article cards.
+// // -----------------------
+// // Send an HTTP GET request to the following address: https://lambda-times-api.herokuapp.com/articles
+// // Study the response data you get back, closely.
+// // You will be creating a card for each article in the response.
+// // This won't be as easy as just iterating over an array though.
+// //
+// // Write a function that takes a single article object and returns the following markup:
+// //
+// // <div class="card">
+// //   <div class="headline">{Headline of article}</div>
+// //   <div class="author">
+// //     <div class="img-container">
+// //       <img src={url of authors image} />
+// //     </div>
+// //     <span>By {author's name}</span>
+// //   </div>
+// // </div>
+// //
+// // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
+// //
+// // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+// const { default: Axios } = require("axios")
+
+
+// const entryPoint= document.querySelector ('.cards-container')
+
+
+// function cardMaker({obj}){
+
+// const card =document.createElement('div')
+// const headline = document.createElement('headline')
+// const author = document.createElement ('author')
+// const imgContainer = document.createElement ('div')
+// const img = document.createElement ('img')
+// const span = document.createElement ('span')
+
+// //give class and text
+
+// card.classList.add ('cards')
+// headline.classList.add('headline')
+// author.classList.add ('author')
+// imgContainer.classList.add ('img-container')
+
+// headline.textContent= obj.headline
+// img.src= obj.authorPhoto
+// span.textContent= obj.authorName
+
+// card.appendChild(headline)
+// card.appendChild(author)
+// author.appendChild(imgContainer)
+// author.appendChild(span)
+// imgContainer.appendChild(img)
+
+// card.addEventListener('click', () => {
+//     card.classList.toggle('selected')
+//   })
+
+
+// return card
+
+// }
+
+
+// axios.get ('https://lambda-times-api.herokuapp.com/articles')
+//     .then(stuff =>{
+//         const infoArray= stuff.data.articles.javascript
+//         entryPoint.appendChild(infoArray)
+//     })
--- components/Tabs.js
+++ components/Tabs.js
@@ -1,3 +1,4 @@
+
 // STEP 2: Create tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-api.herokuapp.com/topics
@@ -9,3 +10,50 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+//   <div class="tabs">
+// <div class="topics">
+// <span class="title">TRENDING TOPICS:</span>
+// <!-- 2. PLACE TABS HERE -->
+// </div>
+// </div>
+
+const { default: Axios } = require("axios")
+
+const entryPoint = document.querySelector ('.span')
+const topics = document.querySelector ('.topics')
+const span = document.querySelector ('.title')
+
+function tabMaker({topics}){
+const newTab = document.createElement ('div')
+newTab.classList.add ('newTab')
+span.appendChild(newTab)    
+
+newTab.textContent= topics
+
+
+// array.forEach(item =>{
+//     const newTab = document.createElement ('div')
+//     newTab.classList.add ('newTab')
+//     span.appendChild.apply(newTab) 
+
+// })
+
+return newTab
+
+}
+
+
+axios.get ('https://lambda-times-api.herokuapp.com/topics')
+.then (stuff =>{
+   
+    const tabArray= stuff.data
+
+    tabArray.forEach(URL=>{
+    const tab=tabMaker({topics})
+    entryPoint.appendChild(tab)
+})
+
+.catch(err =>{
+    console.log('error',err)
+})
+})
